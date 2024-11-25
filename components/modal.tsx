a325cd3a0c37ef05917d31338dbd4ceef5939f1b
Sean Burman <sb9443353@gmail.com>
2024-11-24 18:50:45 -0700

    Added PlainModal component for reuse

---
+++ components/modal.tsx
@@ -0,0 +1,45 @@
+import { theme } from "@/app/_theme";
+import { ReactNode } from "react";
+import { Modal, Pressable, StyleSheet, View } from "react-native";
+
+interface Props {
+    visible: boolean;
+    setVisible: (visible: boolean) => void;
+    children: ReactNode;
+}
+
+export default function PlainModal({ visible, setVisible, children }: Props) {
+    if(!visible) return null;
+    return (
+        <Modal transparent animationType="fade" visible={visible}>
+            <Pressable
+                style={styles.modalContainer}
+                onPress={() => setVisible(false)}
+            >
+                <View style={styles.modalContent}>{children}</View>
+            </Pressable>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 35,
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+        zIndex: 100,
+        ...theme.shadow.small,
+    },
+    modalContent: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
+        borderRadius: 20,
+        backgroundColor: "#FFFFFF",
+        cursor: "auto",
+    }
+});
