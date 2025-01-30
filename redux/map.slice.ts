8c8aac1734e6ef5b2713946f3a822e378ef3c75e
Sean Burman <sb9443353@gmail.com>
2025-01-29 17:01:53 -0700

    Added update and delete map endpoints for redux

--- redux/map.slice.ts
+++ redux/map.slice.ts
@@ -1,3 +1,4 @@
+import { update } from "lodash";
 import { api, AuthToken } from "./api";
 import { CellData, Image } from "./models/image.model";
 import { MapDTO } from "./models/map.model";
@@ -10,19 +11,6 @@ export enum MapError {
 
 export const mapSlice = api.injectEndpoints({
     endpoints: (builder) => ({
-        postMap: builder.mutation<
-            { inserted_id: string },
-            { token: string; map: MapDTO<string> }
-        >({
-            query: (args) => ({
-                url: "/maps",
-                method: "POST",
-                headers: {
-                    ...AuthToken(args.token),
-                },
-                body: args.map,
-            }),
-        }),
         getMapByID: builder.query<
             MapDTO<Image<CellData[][]>[]>,
             { token: string; id: string }
@@ -42,6 +30,41 @@ export const mapSlice = api.injectEndpoints({
                 method: "GET",
                 headers: {
                     ...AuthToken(token),
+                },
+            }),
+        }),
+        postMap: builder.mutation<
+            { inserted_id: string },
+            { token: string; map: MapDTO<string> }
+        >({
+            query: (args) => ({
+                url: "/maps",
+                method: "POST",
+                headers: {
+                    ...AuthToken(args.token),
+                },
+                body: args.map,
+            }),
+        }),
+        updateMap: builder.mutation<
+            void,
+            { token: string; map: MapDTO<string> }
+        >({
+            query: (args) => ({
+                url: "/maps",
+                method: "PATCH",
+                headers: {
+                    ...AuthToken(args.token),
+                },
+                body: args.map,
+            }),
+        }),
+        deleteMap: builder.mutation<void, { token: string; id: string }>({
+            query: (args) => ({
+                url: `/maps/${args.id}`,
+                method: "DELETE",
+                headers: {
+                    ...AuthToken(args.token),
                 },
             }),
         }),
